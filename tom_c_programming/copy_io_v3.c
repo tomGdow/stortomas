3367b8ff2a716a32b00d8fa9ae0fc53f985c4e0c
tomgdow <thomasgdowling@gmail.com>
2016-01-01 01:08:02 +0000

    xubuntu commit

--- tom_c_programming/copy_io_v3.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-/*Copy input to output; 1st version*/
-main () {
-	int c;
-	c = getchar();
-	while ((c = getchar()) != EOF) {
-		putchar(c);
-	}
-}
--- tom_c_programming/eof.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-/* copy input to output; first version */
-
-main () 
-{
-    int c;
-
-        printf("The EOF is %d\n", EOF);
-}
